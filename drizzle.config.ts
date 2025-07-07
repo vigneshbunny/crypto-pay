ea30edb0b0dfd78f8f28f32e6a7c86b392a23c94
vigneshbunny <147530561+vigneshbunny@users.noreply.github.com>
2025-07-07 16:55:02 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,14 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./migrations",
   schema: "./shared/schema.ts",
-  dialect: "sqlite",
+  dialect: "postgresql",
   dbCredentials: {
-    url: "./dev.db",
-  },
+    host: "dpg-d1lpq895pdvs73c9emu0-a.oregon-postgres.render.com",
+    port: 5432,
+    user: "cryptopay_user",
+    password: "RNnUdDl1rpMohUuYt9y1IUdfU8ILRffk",
+    database: "cryptopay",
+    ssl: true
+  }
 });
+
